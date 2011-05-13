3b2f3d40eabb01ee45ee063754bd3981cd3908d7
josh rotenberg <joshrotenberg@gmail.com>
2011-05-13 15:52:07 -0700

    implement basic hello world

--- src/main.cc
+++ src/main.cc
@@ -1,18 +1,53 @@
 #include "zmqpbexample.h"
 
+#include <unistd.h>
+#include <sys/wait.h>
+#include <signal.h>
+
 #include <iostream>
 
 int main(int argc, char** argv) 
 {
   
   GOOGLE_PROTOBUF_VERIFY_VERSION;
-  zmqpbexample z;
-  try {
-    z.run();
-  } catch(...) {
-    std::cerr << "woops!\n";
-    return 1;
-  }
 
+  // run the server
+  zmqpbexample z;
+  pid_t pid;
+
+  pid = fork();
+
+  std::cerr << pid << std::endl;
+  if(pid == 0) {
+    try {
+      z.run();
+    } catch(...) {
+      std::cerr << "woops!\n";
+      return 1;
+    }
+  } else {
+
+    //  Prepare our context and socket
+    zmq::context_t context (1);
+    zmq::socket_t socket (context, ZMQ_REQ);
+
+    std::cout << "Connecting to hello world server..." << std::endl;
+    socket.connect ("tcp://localhost:5555");
+
+    //  Do 10 requests, waiting each time for a response
+    for (int request_nbr = 0; request_nbr != 10; request_nbr++) {
+      zmq::message_t request (6);
+      memcpy ((void *) request.data (), "Hello", 5);
+      std::cout << "Sending Hello " << request_nbr << "..." << std::endl;
+      socket.send (request);
+
+      //  Get the reply.
+      zmq::message_t reply;
+      socket.recv (&reply);
+      std::cout << "Received World " << request_nbr << std::endl;
+    }
+    // kill the server
+    kill(pid, SIGTERM);
+  }
   return 0;
 }
--- src/zmqpbexample.cc
+++ src/zmqpbexample.cc
@@ -11,19 +11,26 @@ zmqpbexample::~zmqpbexample() {
 }
 
 void zmqpbexample::run() {
-  
-  zmq::context_t context(1);
-  zmq::socket_t socket(context, ZMQ_REP);
-  socket.bind("tcp://*:5555");
-    
-  while(true) {
-    zmq::message_t request;
-    socket.recv(&request);
-    std::cerr << "received " << (char *)request.data();
 
-    zmq::message_t reply(2);
-    memcpy((void *) reply.data(), "OK", 2);
-    socket.send(reply);
+  //  Prepare our context and socket
+  zmq::context_t context (1);
+  zmq::socket_t socket (context, ZMQ_REP);
+  socket.bind ("tcp://*:5555");
+
+  while (true) {
+    zmq::message_t request;
+
+    //  Wait for next request from client
+    socket.recv (&request);
+    std::cout << "Received Hello" << std::endl;
+
+    //  Do some 'work'
+    //   sleep (1);
+
+    //  Send reply back to client
+    zmq::message_t reply (5);
+    memcpy ((void *) reply.data (), "World", 5);
+    socket.send (reply);
   }
   
 }
