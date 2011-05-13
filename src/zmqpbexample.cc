b8f2f01ed965f31e4c17c53f02315c890073fc12
josh rotenberg <joshrotenberg@gmail.com>
2011-05-13 11:51:15 -0700

    empty build

---
+++ src/zmqpbexample.cc
@@ -0,0 +1,10 @@
+#include "zmqpbexample.h"
+
+
+int main(int argc, char** argv) 
+{
+
+
+
+  return 0;
+}
---
+++ src/zmqpbexample.h
@@ -0,0 +1,7 @@
+#ifndef __ZMQPBEXAMPLE_H__
+#define __ZMQPBEXAMPLE_H__
+
+
+#include <zmq.hpp>
+
+#endif
